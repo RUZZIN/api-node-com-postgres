ebb24b2f3dd35c8a557ebdbdea41710c6520b179
Ruan Pablo Mesquita <125768983+RUZZIN@users.noreply.github.com>
2023-11-04 09:38:32 -0300

    API node.js com postgres

---
+++ db.js
@@ -0,0 +1,18 @@
+require('dotenv').config();
+const postgres = require('postgres');
+
+let { PGHOST, PGDATABASE, PGUSER, PGPASSWORD, ENDPOINT_ID } = process.env;
+
+const sql = postgres({
+  host: PGHOST,
+  database: PGDATABASE,
+  user: PGUSER,
+  password: PGPASSWORD,
+  port: 5432,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+  connection: {
+    options: `project=${ENDPOINT_ID}`,
+  },
+});
